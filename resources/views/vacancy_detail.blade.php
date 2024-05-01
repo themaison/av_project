2d8198ca385341098c3e12efb5c4ecb4484dd87b
Vladislav Melnichuk <melnichuk1712@mail.ru>
2024-05-01 18:50:28 +0300

    fixed vacancy detail page

--- resources/views/vacancy_detail.blade.php
+++ resources/views/vacancy_detail.blade.php
@@ -34,58 +34,60 @@
         <div class="title">
             <img src="{{  asset('images/vacancy_cover.jpg') }}" alt="preview" class="vacancy-cover">
             <h2>Дизайнер сайтов на Tilda</h2>
+            <button class="outline-btn"><img src="{{  asset('icons/chunk/brush.svg') }}" alt="pencil"><a>редактировать</a></button>
         </div>
 
-        {{-- <div class="breakpoints">
-            <a href="/vacancy_list">вакансии</a> / UI/UX Дизайнер
-        </div> --}}
-        
+        <div class="vacancy-description">
 
-        <div class="vacancy-content">
-            {{-- <img src="{{  asset('images/vacancy_cover.jpg') }}" alt="обложка" class="vacancy-cover">
-            <h2>UI/UX Дизайнер</h2> --}}
-            <button class="outline-btn"><img src="{{  asset('icons/chunk/brush.svg') }}" alt="pencil"><a>редактировать</a></button>
-            
-            <div class="vacancy-description">
-                <div class="inline-boxes">
-                    <div class="salary">
-                        <h3>Заработная плата</h3>
-                        <p>80 000 — 100 000₽</p>
-                    </div>
-                    <div class="company">
-                        <h3>Компания</h3>
-                        <p class="company-detail">
-                            <a>Бассейны Атлантика</a>
-                            <a class="location"><img src="{{  asset('icons/chunk/map-pin.svg') }}" alt="map-pin">Москва</a>
-                        </p>
+            <div class="double-block">
+                <div class="inline-block" id="salary-block">
+                    <h3>Заработная плата</h3>
+                    <p class="tag">80 000 — 100 000₽</p>
+                </div>
+
+                <div class="inline-block" id="company-block">
+                    <h3>Компания</h3>
+                    <div class="company-detail">
+                        <p>Бассейны Атлантика</p>
+                        <p class="tag"><img src="{{  asset('icons/chunk/map-pin.svg') }}" alt="map-pin">Москва</p>
                     </div>
                 </div>
-                <div class="responsibilities">
-                    <h3>Обязанности</h3>
-                    <p> • Разработка сайтов и лендингов на Tilda<br>
-                        • Поддержка существующих сайтов<br>
-                        • Работа с визуальной составляющей сайтов<br>
-                        • Интеграция сайта с Email, Yandex Direct, Google Adwords, VK, Facebook, Instagram, Telegram</p>
-                </div>
-                <div class="requirements">
-                    <h3>Требования</h3>
-                    <p> • Подтвержденный опыт работы на Tilda не менее 3 лет<br>
-                        • Наличие сертификатов на прохождение курсов работы на Tilda и других обучающих программ<br>
-                        • Опыт прототипирования сайта</p>
-                </div>
-                <div class="conditions">
-                    <h3>Условия</h3>
-                    <p> • Разработка сайтов и лендингов на Tilda<br>
-                        • Белая заработная плата (оклад + премия)<br>
-                        • График работы 5/2</p>
-                </div>
-                <div class="skills">
-                    <h3>Навыки</h3>
-                    <div class="tags">
-                        <a>Figma</a> <a>Figma</a> <a>Figma</a>
-                    </div>
+
+            </div>
+
+            <div class="vacancy-content-block">
+                <h3>Обязанности</h3>
+                <p> • Разработка сайтов и лендингов на Tilda<br>
+                    • Поддержка существующих сайтов<br>
+                    • Работа с визуальной составляющей сайтов<br>
+                    • Интеграция сайта с Email, Yandex Direct, Google Adwords, VK, Facebook, Instagram, Telegram</p>
+            </div>
+
+            <div class="vacancy-content-block">
+                <h3>Требования</h3>
+                <p> • Подтвержденный опыт работы на Tilda не менее 3 лет<br>
+                    • Наличие сертификатов на прохождение курсов работы на Tilda и других обучающих программ<br>
+                    • Опыт прототипирования сайта</p>
+            </div>
+
+            <div class="vacancy-content-block">
+                <h3>Условия</h3>
+                <p> • Разработка сайтов и лендингов на Tilda<br>
+                    • Белая заработная плата (оклад + премия)<br>
+                    • График работы 5/2</p>
+            </div>
+
+            <div class="vacancy-content-block">
+                <h3>Навыки</h3>
+                <div class="tags">
+                    <div class="tag">Figma</div>
+                    <div class="tag">Adobe Photoshop</div>
+                    <div class="tag">Tilda</div>
+                    <div class="tag">Adobe Illustrator</div>
+                    <div class="tag">Miro</div>
                 </div>
             </div>
+            
         </div>
     </div>
 @endsection        
