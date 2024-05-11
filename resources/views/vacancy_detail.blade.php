078147904f65f7314fc5d4228cf8a5562702410d
Vladislav Melnichuk <melnichuk1712@mail.ru>
2024-05-12 01:53:38 +0300

    create response modal

--- resources/views/vacancy_detail.blade.php
+++ resources/views/vacancy_detail.blade.php
@@ -3,16 +3,80 @@
 @section('title', $vacancy->title)
 
 @section('content')
+    <link href="{{asset('css/av-form.css?v=').time()}}" rel="stylesheet">
     <link href="{{asset('css/av-cover.css?v=').time()}}" rel="stylesheet">
     <link href="{{asset('css/vacancy_detail.css?v=').time()}}" rel="stylesheet">
 
+    <script>
+        $(document).ready(function() {
+            
+            $('.response-btn').click(function() {
+                $('.av-form').fadeIn().css('display', 'flex');
+                $('.blur-bg').fadeIn();
+            });
+ 
+            $('.cancel-btn, .x-btn').click(function() {
+                $('.av-form').fadeOut();
+                $('.blur-bg').fadeOut();
+            });
+
+            $(document).mouseup(function (e) {
+                var container = $(".av-form");
+                if (container.has(e.target).length === 0){
+                    container.fadeOut();
+                    $('.blur-bg').fadeOut();
+                }
+            });
+        });
+
+    </script>
+
     <div class="content">
+
+        <div class="blur-bg"></div>
 
         <div class="breakpoints" style="--i: 0">
             <a href="{{ url()->previous() }}">вакансии</a>
             <p>/</p>
             <a href="/vacancy_detail/{{  $vacancy->id }}" id="current-page">{{ $vacancy->title }}</a>
         </div>
+
+        <form class="av-form" method="POST" action="/applicant/create_response" enctype="multipart/form-data"  style="display: none">
+            @csrf
+
+            <div class="x-btn">
+                <img src="{{ asset('icons/black/x.svg') }}" alt="icon">
+            </div>
+
+            <div class="form-title">
+                <h3>Отклик</h3>
+
+                <div class="av-icon">
+                    <img src="{{  asset('icons/black/hand-tap.svg') }}" alt="icon">
+                </div>
+
+            </div>
+
+            <div class="inputs-block">
+
+                <div class="input-block">
+                    <label for="message">сопроводительное письмо</label>
+                    <textarea name="message" placeholder="введите текст...">{{ old('message') }}</textarea>
+                    
+                    @error('message')
+                        <p class="error-text">{{ $message }}</p>
+                    @enderror
+
+                </div>
+
+            </div>
+
+            <div class="form-nav">
+                <button type="sybmit" class="fill-btn">откликнуться</button>
+                <div class="outline-btn cancel-btn">отменить</div>
+            </div>
+
+        </form>
 
         <div class="title">
             @if($vacancy->cover)
@@ -28,7 +92,7 @@
             @auth
                 <div class="double-btn" style="--i: 3">
                     @if(auth()->user()->hasRole('applicant'))
-                        <div class="fill-btn">откликнуться</div>
+                        <div class="fill-btn response-btn">откликнуться</div>
                         <a href="/applicant/add_to_favorite" class="outline-btn square-btn"><img src="{{  asset('icons/black/gem.svg') }}" alt="icon"></a>
                     @elseif(auth()->user()->hasRole('recruiter'))
                         <div class="fill-btn"><a href="/vacancies/{{ $vacancy->id }}/edit"><img src="{{  asset('icons/light/pencil.svg') }}" alt="icon">редактировать</a></div>
