5e7f5bdd25bac8241dd17c2ade4be9c8302fa150
Crazy-Lizard <zhenyaivanov.7791@gmail.com>
2024-05-11 14:05:29 +0300

    vacancy updating modal created

--- resources/views/recruiter/recruiter_vacancies.blade.php
+++ resources/views/recruiter/recruiter_vacancies.blade.php
@@ -5,6 +5,7 @@
 @section('content')
     <link href="{{asset('css/av-cover.css?v=').time()}}" rel="stylesheet">
     <link href="{{asset('css/av-list.css?v=').time()}}" rel="stylesheet">
+    <link href="{{asset('css/av-form.css?v=').time()}}" rel="stylesheet">
     <link href="{{ asset('css/recruiter_vacancies.css?v=').time() }}" rel="stylesheet">
 
     <div class="content">
@@ -22,26 +23,6 @@
 
         @isset($vacancies)
             <div class="av-list">
-                
-                {{-- <div class="l-row">
-                    <div class="set">
-
-                        <div class="elem">
-                            <p class="hint-text">30.04.2024 | 21:23</p>
-                        </div>
-
-                        <a href="/" class="elem">
-                            <img src="{{ asset('images/job_prev.jpg') }}" class="av-img">
-                            <p>Наименование вакансии</p>
-                        </a>
-
-                    </div>
-                    
-                    <div class="double-btn">
-                        <button class="outline-btn"><img src="{{ asset('icons/black/pencil.svg') }}" alt="icon">редактировать</button>
-                        <button class="outline-btn square-btn"><img src="{{ asset('icons/black/trash.svg') }}" alt="icon"></button>
-                    </div>
-                </div> --}}
                 
                 @forelse ($vacancies as $vacancy)
                     <div class="l-row">
@@ -68,10 +49,15 @@
 
                         <div class="double-btn">
 
-                            <div class="outline-btn" id="v-edit-{{  $vacancy->id }}">
+                            {{-- <div class="outline-btn" id="v-edit-{{ $vacancy->id }}">
                                 <img src="{{ asset('icons/black/pencil.svg') }}" alt="icon">
                                 редактировать
-                            </div>
+                            </div> --}}
+
+                            <button type="button" class="outline-btn" data-toggle="modal" data-target="#edit-modal">
+                                <img src="{{ asset('icons/black/pencil.svg') }}" alt="icon">
+                                редактировать
+                            </button>
  
                             <form action="/recruiter_vacancies/vacancy_delete/{{  $vacancy->id }}" method="POST">
                                 @csrf
@@ -87,8 +73,43 @@
                 @empty
                     <p>У вас пока нет вакансий.</p>
                 @endforelse
-                
+
             </div>
         @endisset
+                
+        <div class="edit-modal" id="edit-modal">
+            <button type="submit" class="x-btn">
+                <img src="{{ asset('icons/black/x.svg') }}" alt="icon">
+            </button>
+            <form class="av-form" method="PUT" action="">
+                @csrf
+
+                <div class="form-title">
+                    <h3>Редактировать вакансию</h3>
+                </div>
+
+                <div class="inputs-block">
+                    <div class="input-block">
+                        <label for="title">название вакансии</label>
+                        <input type="text" name="title" value="{{ $vacancy->title }}" placeholder="введите текст...">
+                        
+                        @error('title')
+                            <p class="error-text">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                    <div class="input-block">
+                        <label for="company">компания (ИП)</label>
+                        <input type="text" name="company"  value="{{ $vacancy->company }}" placeholder="введите текст...">
+                        
+                        @error('company')
+                            <p class="error-text">{{ $message }}</p>
+                        @enderror
+                    </div>
+
+                </div>
+            </form>
+        </div>
+
     </div>
 @endsection
